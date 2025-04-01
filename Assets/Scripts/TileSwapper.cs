f74f82bdac54c4bd8e98503b02e67a2ef41bc00c
Qiaoya Fang <fangqi@shernet.sheridancollege.ca>
2025-04-01 17:50:38 -0400

    grass coding gym

--- Assets/Scripts/TileSwapper.cs
+++ Assets/Scripts/TileSwapper.cs
@@ -2,11 +2,14 @@ using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class TileSwapper : MonoBehaviour
 {
 
-    public CinemachineImpulseSource impulseScource;
+    public Tilemap tilemap;
+    public Tile grass;
+    public Tile stone;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +19,19 @@ public class TileSwapper : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int gridPos = tilemap.WorldToCell(mousePos);
+
+            if (tilemap.GetTile(gridPos) == stone)
+            {
+                Debug.Log("You Can walk on the stone");
+            }
+            else
+            {
+                Debug.Log("You Can't walk on the grass");
+            }
+        } 
     }
 }
